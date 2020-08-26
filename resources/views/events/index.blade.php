f3f174aa328732e844663914e428fdb87e9bea5a
Alejandro <alejandroacho@hotmail.com>
2020-08-26 11:02:12 +0200

    Revert "6 | Event create function made"
    This reverts commit ed39c172d995528c7334f3a0d4aa1aa05d1d7b44.

--- resources/views/events/index.blade.php
+++ resources/views/events/index.blade.php
@@ -34,17 +34,8 @@
                 },
                 dateClick:function(info){
                     $('#date').val(info.dateStr);
-                    $('#createModal').modal('toggle');
-                },
-                eventClick:function(info){
-                    $('#id').val(info.event.id);   
-                    $('#title').val(info.event.title);   
-                    $('#date').val(info.event.start);
-                    $('#start').val(info.event.start);
-                    $('#description').val(info.event.extendedProps.description);
-                    $('#color').val(info.event.backgroundColor);
-                    console.log(info.event.id);
-                    $('#createModal').modal('toggle');
+                    $('#exampleModal').modal('toggle');
+                    calendar.addEvent({title:"Evento x", date:info.dateStr});
                 },
                 events:"{{ url ('event/show') }}"
             });
@@ -79,8 +70,6 @@
                     data:eventObj,
                     success: function(msg){
                         console.log(msg);
-                        $('#createModal').modal('toggle');
-                        calendar.refetchEvents();
                     },
                     error: function(){
                         alert("Hubo un error");
@@ -97,7 +86,7 @@
         <div id="calendar"></div>
     </div>
 
-    <div class="modal fade" id="createModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
         <div class="modal-dialog">
             <div class="modal-content">
             <div class="modal-header">
@@ -107,7 +96,6 @@
                 </button>
             </div>
             <div class="modal-body">
-                <input type="hidden" name="id" id="id">
                 Titulo
                 <input type="text" name="title" id="title"> <br>
                 Fecha
@@ -123,11 +111,10 @@
                 <button id="btnAdd" type="button" class="btn btn-success">Agregar</button>
                 <button id="btnEdit" type="button" class="btn btn-warning">Editar</button>
                 <button id="btnDelete" type="button" class="btn btn-danger">Borrar</button>
-                <button id="btnCancel" type="button" class="btn btn-secondary" data-dismiss="modal">Cancelar</button>
+                <button id="btnCancel" type="button" class="btn btn-secondary">Cancelar</button>
             </div>
             </div>
         </div>
     </div>
-
 
 @endsection
